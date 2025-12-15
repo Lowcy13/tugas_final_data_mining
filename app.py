41ae8fa3ee81fcda55e4810297aa689d63fe4c3a
Gafur Rahim <gafursrt88@gmail.com>
2025-12-15 13:33:37 +0800

    update

--- app.py
+++ app.py
@@ -108,10 +108,10 @@ if submit:
 
     # Nilai akhir gabungan
     nilai_akhir = (
-    0.35 * uts +          # 35% UTS
+    0.30 * uts +          # 35% UTS
     0.40 * uas +          # 40% UAS (paling berpengaruh)
     0.10 * tugas +        # 15% Tugas
-    0.15 * nilai_kehadiran # 15% Kehadiran
+    0.20 * nilai_kehadiran # 15% Kehadiran
 )
 
 
